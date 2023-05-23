47fbe950199a2dc876d9c4376d348c01539d3ee9
Maria Kadyrova <[maria_kadyrova@mail.ru]>
2023-05-23 16:44:15 +0300

    add css component, add state with user, users

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,3 +1,42 @@
-export const Messages = () => {
-  return <h1>Это страница с сообщениями</h1>;
+const TableRow = (props) => {
+  return (
+    <tr>
+      <th scope="row">{props.index + 1}</th>
+      <td>{props.name}</td>
+      <td>{props.lastname}</td>
+    </tr>
+  );
+};
+
+export const Messages = (props) => {
+  let users = props.function();
+  let usersCount = Object.keys(users).length;
+  let usersRow = [];
+  for (let i = 0; i < usersCount; i++) {
+    usersRow.push(
+      <TableRow
+        index={i}
+        key={i}
+        name={users[i].name}
+        lastname={users[i].lastname}
+      />
+    );
+  }
+  return (
+    <>
+      <h2 className="text-center">Мои друзья:</h2>
+      <div className="row">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Имя</th>
+              <th scope="col">Фамилия</th>
+            </tr>
+          </thead>
+          <tbody>{usersRow}</tbody>
+        </table>
+      </div>
+    </>
+  );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,28 @@
-export const Profile = () => {
-  return <h1>Это страница с профилем</h1>;
+import styles from "./Profile.module.css";
+
+export const Profile = (props) => {
+  let user = props.function();
+  return (
+    <div className="row">
+      <div className="col-sm-4">
+        <img src={user.avatar} alt="" width="100%" />
+      </div>
+      <div className="col-sm-8">
+        <h2 className={styles.id}>
+          ID: <span>{user.id}</span>
+        </h2>
+        <h2 className={styles.name}>
+          Имя и Фамилия:{" "}
+          <span>
+            {user.name} {user.lastname}
+          </span>
+        </h2>
+        <p className={styles.mail}>
+          E-mail: <span>{user.email}</span>
+        </p>
+        <p className={styles.text}>{user.about}</p>
+        <p style={{ fontWeight: 700 }}>Небольшой текст</p>
+      </div>
+    </div>
+  );
 };
